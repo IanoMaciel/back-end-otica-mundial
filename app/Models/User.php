6af554d3a842761e497b2b6692387206795da67e
IanoMaciel <ianomaciel6385@gmail.com>
2024-12-16 10:22:29 -0400

    feat: criação da classe users

--- app/Models/User.php
+++ app/Models/User.php
@@ -18,7 +18,11 @@ class User extends Authenticatable
      * @var array<int, string>
      */
     protected $fillable = [
-        'name',
+        'first_name',
+        'last_name',
+        'is_admin',
+        'is_manager',
+        'discount',
         'email',
         'password',
     ];
@@ -41,4 +45,45 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function rules(bool $update = false): array {
+        return [
+            'first_name' => 'required|string|min:3|max:30',
+            'last_name' => 'required|string|max:60',
+            'is_admin' => 'boolean',
+            'is_manager' => 'boolean',
+            'discount' => 'numeric',
+            'email' => $update ? 'required|string|email' : 'required|string|email|unique:users',
+            'password' => 'required|string|confirmed|min:8',
+        ];
+    }
+
+    public function messages(): array {
+        return [
+            'first_name.required' => 'O campo Nome é obrigatório.',
+            'first_name.string' => 'O campo Nome deve conter apenas caracteres alfabéticos.',
+            'first_name.min' => 'O campo Nome deve ter no mínimo 3 caracteres.',
+            'first_name.max' => 'O campo Nome deve ter no máximo 30 caracteres.',
+
+            'last_name.required' => 'O campo Sobrenome é obrigatório.',
+            'last_name.string' => 'O campo Sobrenome deve conter apenas caracteres alfabéticos.',
+            'last_name.max' => 'O campo Sobrenome deve ter no máximo 60 caracteres.',
+
+            'is_admin.boolean' => 'O campo Administrador deve ser verdadeiro ou falso.',
+            'is_manager.boolean' => 'O campo Gerente deve ser verdadeiro ou falso.',
+
+            'discount.numeric' => 'O campo Desconto deve ser um valor numérico.',
+
+            'email.required' => 'O campo E-mail é obrigatório.',
+            'email.string' => 'O campo E-mail deve ser uma string.',
+            'email.email' => 'O campo E-mail deve ser um endereço de e-mail válido.',
+            'email.unique' => 'O endereço de e-mail já está em uso.',
+
+            'password.required' => 'O campo Senha é obrigatório.',
+            'password.string' => 'O campo Senha deve ser uma string.',
+            'password.confirmed' => 'A confirmação da senha não corresponde.',
+            'password.min' => 'O campo Senha deve ter no mínimo 8 caracteres.',
+        ];
+    }
+
 }
