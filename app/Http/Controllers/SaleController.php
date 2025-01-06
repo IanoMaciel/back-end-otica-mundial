3edaf16a57b774e6b662f43f059ddd302fa0a43b
IanoMaciel <ianomaciel6385@gmail.com>
2025-01-06 15:58:01 -0400

    (#6): em desenvolvimento manter vendas

---
+++ app/Http/Controllers/SaleController.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class SaleController extends Controller {
+
+}
---
+++ app/Models/Sale.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Sale extends Model {
+    use HasFactory;
+    protected $fillable = [
+        'customer_id',
+        'user_id',
+        'payment_method_id',
+        'status',
+        'discount',
+        'total_amount',
+    ];
+}
