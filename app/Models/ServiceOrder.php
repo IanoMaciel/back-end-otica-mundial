346aaad3bf1cb6b7f151db63cd1a86db0fd815df
IanoMaciel <ianomaciel6385@gmail.com>
2025-02-20 16:19:55 -0400

    feat: migração

--- app/Models/ServiceOrder.php
+++ app/Models/ServiceOrder.php
@@ -8,12 +8,35 @@ use Illuminate\Database\Eloquent\Model;
 class ServiceOrder extends Model {
     use HasFactory;
     protected $fillable = [
-        'number_ata'
+        'sale_id',
+        'number_ata',
+        'delivery',
+        'spherical_left',
+        'spherical_right',
+        'cylindrical_left',
+        'cylindrical_right',
+        'axis_left',
+        'axis_right',
+        'dnp_left',
+        'dnp_right',
+        'height_left',
+        'height_right',
+        'addition_left',
+        'addition_right',
+        'bridge',
+        'major_horizontal',
+        'minor_horizontal',
+        'dnp_v_left',
+        'dnp_v_right',
+        'alt_left',
+        'alt_right'
     ];
 
     public function rules(): array {
         return [
-            'number_ata' => 'required|exists:sales,number_ata'
+            'sale_id' => 'sometimes|exists|sales,id',
+            'number_ata' => 'required|exists:sales,number_ata',
+            'delivery' => 'sometimes|date',
         ];
     }
 
--- database/migrations/2025_02_18_141151_create_service_orders_table.php
+++ database/migrations/2025_02_18_141151_create_service_orders_table.php
@@ -15,7 +15,32 @@ class CreateServiceOrdersTable extends Migration
     {
         Schema::create('service_orders', function (Blueprint $table) {
             $table->id();
+
+            $table->unsignedBigInteger('sale_id')->nullable();
+            $table->foreign('sale_id')->references('id')->on('sales')->nullOnDelete();
+
             $table->string('number_ata')->nullable();
+            $table->date('delivery')->nullable();
+            $table->string('spherical_left')->nullable();
+            $table->string('spherical_right')->nullable();
+            $table->string('cylindrical_left')->nullable();
+            $table->string('cylindrical_right')->nullable();
+            $table->string('axis_left')->nullable();
+            $table->string('axis_right')->nullable();
+            $table->string('dnp_left')->nullable();
+            $table->string('dnp_right')->nullable();
+            $table->string('height_left')->nullable();
+            $table->string('height_right')->nullable();
+            $table->string('addition_left')->nullable();
+            $table->string('addition_right')->nullable();
+            $table->string('bridge')->nullable();
+            $table->string('major_horizontal')->nullable();
+            $table->string('minor_horizontal')->nullable();
+            $table->string('dnp_v_left')->nullable();
+            $table->string('dnp_v_right')->nullable();
+            $table->string('alt_left')->nullable();
+            $table->string('alt_right')->nullable();
+
             $table->timestamps();
         });
     }
