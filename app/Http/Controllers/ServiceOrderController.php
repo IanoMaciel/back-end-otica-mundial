b99a676aa16064eb3785ca851320733303030ac4
IanoMaciel <ianomaciel6385@gmail.com>
2025-02-23 21:52:00 -0400

    feat(#18): criação dos métodos show, update e destroy

--- app/Http/Controllers/ServiceOrderController.php
+++ app/Http/Controllers/ServiceOrderController.php
@@ -41,4 +41,61 @@ class ServiceOrderController extends Controller {
             ]);
         }
     }
+
+    public function show(int $id): JsonResponse {
+        $serviceOrder = $this->serviceOrder->query()->find($id);
+
+        if (!$serviceOrder) {
+            return response()->json([
+                'error' => 'A ordem de serviço informada não existe na base de dados.',
+            ], 404);
+        }
+
+        return response()->json($serviceOrder);
+    }
+
+    public function update(Request $request, int $id): JsonResponse {
+        $serviceOrder = $this->serviceOrder->query()->find($id);
+
+        if (!$serviceOrder) {
+            return response()->json([
+                'error' => 'A ordem de serviço informada não existe na base de dados.',
+            ], 404);
+        }
+
+        $validatedData = $request->validate(
+            $this->serviceOrder->rules(),
+            $this->serviceOrder->messages()
+        );
+
+        try {
+            $serviceOrder->update($validatedData);
+            return response()->json($serviceOrder);
+        } catch (\Throwable $th) {
+            return response()->json([
+                'error' => 'Erro ao processar a solicitação.',
+                'message' => $th->getMessage()
+            ], 500);
+        }
+    }
+
+    public function destroy(int $id): JsonResponse {
+        $serviceOrder = $this->serviceOrder->query()->find($id);
+
+        if (!$serviceOrder) {
+            return response()->json([
+                'error' => 'A ordem de serviço informada não existe na base de dados.',
+            ], 404);
+        }
+
+        try {
+            $serviceOrder->delete();
+            return response()->json(null, 204);
+        } catch (\Throwable $th) {
+            return response()->json([
+                'error' => 'Erro ao processar a solicitação.',
+                'message' => $th->getMessage()
+            ], 500);
+        }
+    }
 }
