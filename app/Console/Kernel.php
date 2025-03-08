effa7ca05dff4bd11f5f823177778f6f839c72ec
IanoMaciel <ianomaciel6385@gmail.com>
2025-03-08 11:02:52 -0400

    feat(#20): troca de status automatizados

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -2,31 +2,21 @@
 
 namespace App\Console;
 
+use App\Console\Commands\UpdatePromotionStatus;
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 
-class Kernel extends ConsoleKernel
-{
-    /**
-     * Define the application's command schedule.
-     *
-     * @param  \Illuminate\Console\Scheduling\Schedule  $schedule
-     * @return void
-     */
-    protected function schedule(Schedule $schedule)
-    {
-        // $schedule->command('inspire')->hourly();
+class Kernel extends ConsoleKernel {
+    protected $commands = [
+        UpdatePromotionStatus::class,
+    ];
+
+    protected function schedule(Schedule $schedule): void {
+        $schedule->command('promotion:update-status')->everyMinute();
     }
 
-    /**
-     * Register the commands for the application.
-     *
-     * @return void
-     */
-    protected function commands()
-    {
+    protected function commands(): void {
         $this->load(__DIR__.'/Commands');
-
         require base_path('routes/console.php');
     }
 }
