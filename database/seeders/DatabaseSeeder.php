d25467bdd5c38ea222826bec27458dae1d2c3add
IanoMaciel <ianomaciel6385@gmail.com>
2025-03-28 08:32:46 -0400

    refactor: adicionado comentários

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -22,22 +22,21 @@ use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder {
     public function run(): void {
-        User::factory(1)->create(); // não remover
-
+        //User::factory(1)->create(); // não remover
         Brand::factory()->count(4)->create(); // não remover
         Material::factory()->count(3)->create(); // não remover
 
         // Suppliers
-        Supplier::factory(3)->create();
+        //Supplier::factory(3)->create();
 
         // Products and services
-        Frame::factory(100)->create();
-        Service::factory(20)->create();
+        // Frame::factory(100)->create();
+        // Service::factory(20)->create();
 
         // Customers
-        Agreement::factory()->count(4)->create();
-        Customer::factory()->count(100)->create();
-        Address::factory(100)->create();
+        // Agreement::factory()->count(4)->create();
+        // Customer::factory()->count(100)->create();
+        // Address::factory(100)->create();
 
         PaymentMethod::factory()->count(6)->create(); // Não remover
         FormPayment::factory()->count(4)->create(); // Não remover
