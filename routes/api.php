6e0380bf20182a096a1111648925832e28f5c6cc
IanoMaciel <ianomaciel6385@gmail.com>
2025-04-30 10:42:06 -0400

    feat: criação do endpoint

--- routes/api.php
+++ routes/api.php
@@ -70,8 +70,11 @@ Route::middleware('auth:sanctum')->group(function () {
     // financial
     Route::apiResource('payment-method', 'PaymentMethodController'); //Método de pagamento principal
     Route::apiResource('form-payment', 'FormPaymentController'); //Forma de pagamento secundária
+
     Route::apiResource('card', 'CardController'); //juros do cartão
+    Route::get('find-all', 'CardController@all'); //juros do cartão
     Route::delete('card-delete-multiple', 'CardController@deleteAll'); //juros do cartão
+
     Route::apiResource('discount', 'DiscountController'); // tipos de descontos
 
     Route::apiResource('sale', 'SaleController');
