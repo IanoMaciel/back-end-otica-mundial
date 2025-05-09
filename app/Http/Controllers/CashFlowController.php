f6ff2b6d63b198fa1d3e3d898bc29ba773d5c784
IanoMaciel <ianomaciel6385@gmail.com>
2025-05-09 11:15:12 -0400

    feat: criação do método cashflow

--- app/Http/Controllers/CashFlowController.php
+++ app/Http/Controllers/CashFlowController.php
@@ -18,53 +18,72 @@ class CashFlowController extends Controller {
     public function cashFlow(Request $request): JsonResponse {
         $date = $request->input('date') ? Carbon::parse($request->input('date')) : Carbon::today();
 
-        $query = $this->sale
-            ->query()
-            ->with([
-                'customer',
-                'user',
-                'paymentMethod',
-                'frames',
-                'services',
-                'lenses',
-                'creditCards',
-                'paymentCredits',
-                'combinedPayment',
-                'cashPromotions.formPayment',
-                'cashPromotions.promotion',
-                'creditPromotions.promotion'
-            ])
-            ->whereDate('created_at', $date);
+        $filter = $request->query('filter');
+        if ($filter === 'expenses') {
+            $expense = Expense::query()
+                ->with(['categoryExpenses'])
+                ->whereDate('date_proof', $date);
 
-        if ($status = $request->input('status')) {
-            $query->where(function ($q) use ($status) {
-                $q->where('status', 'LIKE', "%$status");
-            });
+            if ($status = $request->input('status')) {
+                $expense->whereHas('categoryExpenses', function ($query) use ($status) {
+                    $query->where('category_expense', 'LIKE', "%$status%");
+                });
+            }
+
+            $perPage = $request->get('per_page', 10);
+            $data = $expense->paginate($perPage);
+        } else {
+            $query = $this->sale
+                ->query()
+                ->with([
+                    'customer',
+                    'user',
+                    'paymentMethod',
+                    'frames',
+                    'services',
+                    'lenses',
+                    'creditCards',
+                    'paymentCredits',
+                    'combinedPayment',
+                    'cashPromotions.formPayment',
+                    'cashPromotions.promotion',
+                    'creditPromotions.promotion'
+                ])
+                ->whereDate('date_sale', $date);
+
+            if ($status = $request->input('status')) {
+                $query->where(function ($q) use ($status) {
+                    $q->where('status', 'LIKE', "%$status");
+                });
+            }
+
+            $perPage = $request->get('per_page', 10);
+            $data = $query->paginate($perPage);
         }
 
-        $perPage = $request->get('per_page', 10);
-        $sales = $query->paginate($perPage);
+        $gains = $this->sale->query()->whereDate('date_sale', $date);
 
-        $gains = $query->where('status', 'Pago')->sum('total_amount');
-        $expenses = Expense::query()
-            ->with('categoryExpenses')
-            ->where('')
-            ->whereDate('date_proof', $date);
+        $totalGains = $gains->where('status', 'Pago')->sum('total_amount');
 
+        $totalExpenses = Expense::query()
+            ->whereDate('date_proof', $date)
+            ->sum('total_amount');
 
-        $pending = $query->where('status', 'Pendente')->sum('total_amount');
-        $cancel = $query->where('status', 'Cancelado')->sum('total_amount');
-
-        $perPage = $request->get('per_page', 10);
+        $totalWithdrawal = Expense::query()
+            ->whereHas('categoryExpenses', function ($query) {
+                $query->where('category_expense', 'Retirada');
+            })
+            ->whereDate('date_proof', $date)
+            ->sum('total_amount');
 
         return response()->json([
             'panel' => [
-                'gains' => $gains,
-                'withdrawal' => 12300.00,
-                'expenses' => 123213,
-                'balance' => 123123
+                'gains' => $totalGains,
+                'withdrawal' => $totalWithdrawal,
+                'expenses' => $totalExpenses,
+                'balance' => $totalGains - $totalWithdrawal - $totalExpenses,
             ],
-            'sales' => $sales
+            'data' => $data
         ]);
     }
 }
